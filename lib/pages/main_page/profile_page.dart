a6015740efee31e31753c3f65038bd5cdf16067f
Pan Myint Mo <panmyintmo9@gmail.com>
2026-03-23 20:24:11 +0630

    added setting in bottom menu & change ui in profile

--- lib/pages/main_page/profile_page.dart
+++ lib/pages/main_page/profile_page.dart
@@ -72,16 +72,24 @@ class _ProfilePageState extends State<ProfilePage> {
                 children: [
                   const Icon(Icons.person_outline, size: 100, color: Colors.grey),
                   const SizedBox(height: kSP20x),
-                  EasyTextWidget(
-                    text: 'Guest User',
-                    fontWeight: FontWeight.w600,
-                    fontSize: kFontSize21x,
+                  Align(
+                    alignment: Alignment.center,
+                    child: EasyTextWidget(
+                      text: 'Guest User',
+                      textAlign: TextAlign.right,
+                      fontWeight: FontWeight.w600,
+                      fontSize: kFontSize21x,
+                    ),
                   ),
                   const SizedBox(height: kSP20x),
-                  EasyTextWidget(
-                    text: 'You are using the app as a guest.',
-                    fontWeight: FontWeight.w400,
-                    fontSize: kFontSize16x,
+                  Align(
+                    alignment: Alignment.center,
+                    child: EasyTextWidget(
+                      text: 'You are using the app as a guest.',
+                      textAlign: TextAlign.right,
+                      fontWeight: FontWeight.w400,
+                      fontSize: kFontSize16x,
+                    ),
                   ),
                 ],
               ),
